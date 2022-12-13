9dff909a05deecd2a56a915d052eaa2a070008cb
hujiang <a15174027322@icloud.com>
2022-12-13 16:42:06 +0800

    update for benchmark

--- benchmark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-address=$1
-port=$(kubectl get svc new-cache-chain-node0-nodeport -ncita |grep '8000' | awk '{print $5}')
-port=${port: -9}
-port=${port: 0:5}
-sed  -i "" 's/^  "to": \(.*\)/  "to": "'$address'",/' /Users/naughtydog/Desktop/benchmark/data.json
-echo "ab -n 8192 -c 1000 -T application/json -p /Users/naughtydog/Desktop/benchmark/data.json http://192.168.10.120:$port/api/sendTx"
-ab -n 8192 -c 1000 -T application/json -p /Users/naughtydog/Desktop/benchmark/data.json http://192.168.10.120:$port/api/sendTx
--- redeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl delete -f /Users/naughtydog/CLionProjects/cache/deploy.yml -ncita
-kubectl delete pvc datadir-new-cache-chain-node0-0 -ncita
-kubectl apply -f /Users/naughtydog/CLionProjects/cache/deploy.yml -ncita
-port=$(kubectl get svc new-cache-chain-node0-nodeport -ncita |grep '8000' | awk '{print $5}')
-port=${port: -9}
-port=${port: 0:5}
-echo $port
